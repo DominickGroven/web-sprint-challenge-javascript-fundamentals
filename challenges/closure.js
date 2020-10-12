fd845879ceb48aab1bb53cefaf0d524f5935710f
Dominick Groven <36062458+DominickGroven@users.noreply.github.com>
2020-10-11 19:02:15 -0700

    Finished up prototypes

--- challenges/closure.js
+++ challenges/closure.js
@@ -31,5 +31,5 @@ function summation(num) {
   return totalNum;
 };
 
-console.log(summation(4));
+console.log(summation(4));g
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
